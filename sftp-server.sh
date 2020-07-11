238451e82ff9589b7fb30210d179a4bc876a7cb1
ivanjrt <44326428+ivanjrt@users.noreply.github.com>
2020-07-11 02:33:25 -0500

    Update sftp-server.sh

--- sftp-server.sh
+++ sftp-server.sh
@@ -3,12 +3,12 @@ mkdir -p /data/sftp
 chmod 701 /data
 groupadd sftpusers
 read -p "Enter a SFTP User to be added: " userList
-useradd -g sftpusers -d /upload -s /sbin/nologin  userList
+useradd -g sftpusers -d /upload -s /sbin/nologin  "$userList"
 echo "Enter His password: "
-passwd userList
-mkdir -p /data/userList/upload
-chown -R root:sftpusers /data/userList
-chown -R userList:sftpusers /data/userList/upload
+passwd "$userList"
+mkdir -p /data/"$userList"/upload
+chown -R root:sftpusers /data/"$userList"
+chown -R "$userList":sftpusers /data/"$userList"/upload
 
 echo Match Group sftpusers >> /etc/ssh/sshd_config
 echo ChrootDirectory /data/%u >> /etc/ssh/sshd_config
